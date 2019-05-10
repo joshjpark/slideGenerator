659fb48776d85d362cc6cf5294aa686b68a96c6c
epido1 <joshua-explorer@hotmail.com>
2019-05-10 01:33:53 -0700

    Create slowstart.py

---
+++ slowstart.py
@@ -0,0 +1,109 @@
+#(1) Standard set up for Google Slides API 
+from __future__ import print_function
+import uuid
+
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+gen_uuid = lambda : str(uuid.uuid4())  # generate random, unique UUID string from a standard library
+
+SCOPES = 'https://www.googleapis.com/auth/presentations', # read write authorization
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+SLIDES = discovery.build('slides', 'v1', http=creds.authorize(Http())) # create the service end point for the slides API 
+'''create a brand new presentation, then grab the title and subtitle textbox IDs from the default title slide from the API response from lines 23 to 25'''
+print('** Create new slide deck & set up object IDs')
+rsp = SLIDES.presentations().create(
+        body={'title': 'Adding text & shapes DEMO'}).execute()
+deckID = rsp['presentationId']
+titleSlide  = rsp['slides'][0]      # title slide object IDs
+titleID     = titleSlide['pageElements'][0]['objectId']
+subtitleID  = titleSlide['pageElements'][1]['objectId']
+'''create randomly generated object ID's as in line 9'''
+mpSlideID   = gen_uuid()            # mainpoint IDs
+mpTextboxID = gen_uuid()
+smileID     = gen_uuid()            # shape IDs
+str24ID     = gen_uuid()
+arwbxID     = gen_uuid()
+
+'''NOTE: Why should you generated using gen_uuid() rather than resorting to default 
+values of API? Because you can use these for the remaining requests in a single batch. '''
+
+'''request to create a new slide for our shapes'''
+print('** Create "main point" slide, add text & interesting shapes')
+reqs = [
+    # create new "main point" layout slide, giving slide & textbox IDs; the placeholder ID mapping
+    # is an array to provide the IDs for the elements that are part of that layout. In this example, we only have one element because it has only one largish textbox. 
+    {'createSlide': {
+        'objectId': mpSlideID,
+        'slideLayoutReference': {'predefinedLayout': 'MAIN_POINT'},
+        'placeholderIdMappings': [{
+            'objectId': mpTextboxID,
+            'layoutPlaceholder': {'type': 'TITLE', 'index': 0}
+        }],
+    }},
+    # add title & subtitle to title slide; add text to main point slide textbox
+    {'insertText': {'objectId': titleID,     'text': 'Hello world!'}},
+    {'insertText': {'objectId': subtitleID,  'text': 'via the Google Slides API'}},
+    {'insertText': {'objectId': mpTextboxID, 'text': 'text & shapes'}},
+    # create smiley face
+    # NOTE: take a look at the Page Elements page in the documentation as well as the Transforms Concept Guide for more details. 
+    {'createShape': {
+        'objectId': smileID,
+        'shapeType': 'SMILEY_FACE',
+        'elementProperties': {
+            "pageObjectId": mpSlideID,
+            'size': {
+                'height': {'magnitude': 3000000, 'unit': 'EMU'},
+                'width':  {'magnitude': 3000000, 'unit': 'EMU'}
+            },
+            'transform': {
+                'unit': 'EMU', 'scaleX': 1.3449, 'scaleY': 1.3031,
+                'translateX': 4671925, 'translateY': 450150,
+            },
+        },
+    }},
+    # create 24-point star
+    {'createShape': {
+        'objectId': str24ID,
+        'shapeType': 'STAR_24',
+        'elementProperties': {
+            "pageObjectId": mpSlideID,
+            'size': {
+                'height': {'magnitude': 3000000, 'unit': 'EMU'},
+                'width':  {'magnitude': 3000000, 'unit': 'EMU'}
+            },
+            'transform': {
+                'unit': 'EMU', 'scaleX': 0.7079, 'scaleY': 0.6204,
+                'translateX': 2036175, 'translateY': 237350,
+            },
+        },
+    }},
+    # create double left & right arrow w/textbox
+    {'createShape': {
+        'objectId': arwbxID,
+        'shapeType': 'LEFT_RIGHT_ARROW_CALLOUT',
+        'elementProperties': {
+            "pageObjectId": mpSlideID,
+            'size': {
+                'height': {'magnitude': 3000000, 'unit': 'EMU'},
+                'width':  {'magnitude': 3000000, 'unit': 'EMU'}
+            },
+            'transform': {
+                'unit': 'EMU', 'scaleX': 1.1451, 'scaleY': 0.4539,
+                'translateX': 1036825, 'translateY': 3235375,
+            },
+        },
+    }},
+    # add text to all 3 shapes
+    {'insertText': {'objectId': smileID, 'text': 'Put the nose somewhere here!'}},
+    {'insertText': {'objectId': str24ID, 'text': 'Count 24 points on this star!'}},
+    {'insertText': {'objectId': arwbxID, 'text': "An uber bizarre arrow box!"}},
+]
+SLIDES.presentations().batchUpdate(body={'requests': reqs},
+        presentationId=deckID).execute()
+print('DONE')
