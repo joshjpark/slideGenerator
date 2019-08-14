7755d3bffc2f97d744e83d689b3aa74a45eec748
Juyoung Park <james.park.victoria@gmail.com>
2019-08-13 22:33:09 -0700

    Initial implementation of Presentation and Page classes

---
+++ Page.py
@@ -0,0 +1,15 @@
+class Page:
+    def __init__(self):
+        self.pageElements = []
+
+    # TODO
+    def addPageElement(self):
+        return
+
+    # TODO
+    def modifyPageElement(self):
+        return
+
+    # TODO
+    def deletePageElement(self):
+        return
