1ef18c927bcb4ee733adde00d71e1e11882cbfdc
joshjpark <joshua-explorer@hotmail.com>
2020-04-25 22:29:12 -0700

    deleted extraneous files

--- bg.py
+++ bg.py
@@ -6,11 +6,12 @@ with open('background.json') as jsonFile:
 
 def findRandomImg():
     rnum = random.randint(0, 2)
-
     if rnum == 0:
         return (random.choice(data['Birds']))
-    elif random == 1:
-        return (random.choice(data['Nightsky']))
+    elif rnum == 1:
+        return (random.choice(data['NightSky']))
     else:
         return (random.choice(data['Sky']))
 
+def fetchImg(category, index):
+    return data[category][index]
--- start.py
+++ start.py
@@ -5,16 +5,15 @@ import bg
 
 # sn.createVerses('James', '1', '8', '1', '15', '1', '9', 'James Message Title', 'https://upload.wikimedia.org/wikipedia/commons/8/85/Haliaeetus_pelagicus_%28Rausu%2C_Japan%29.jpg','https://upload.wikimedia.org/wikipedia/commons/8/85/Haliaeetus_pelagicus_%28Rausu%2C_Japan%29.jpg')
 
-# randomly feed background images 
 sn = Slides()
 sn.createTitle('April 23rd 2020', bg.findRandomImg())
-sn.createHymn(71, "hymn", bg.findRandomImg())
+sn.createHymn(360, "hymn", bg.findRandomImg())
 sn.createTransition('Silent Prayer', bg.findRandomImg())
 sn.createRecital('Creed', bg.findRandomImg())
 sn.createHymn(9, "hymn", bg.findRandomImg())
 sn.createTransition("Representative prayer", bg.findRandomImg())
 # sn.createVerses('Joshua', '1', '6', '2', '6', '1', '8', "Be strong and courageous",bg.findRandomImg(), bg.findRandomImg())
-sn.createVerses('James', '1', '8', '1', '15', '1', '9', 'James message', bg.findRandomImg(), bg.findRandomImg())
+sn.createVerses('Genesis', '1', '1', '1', '7', '1', '1', 'God created the Heavens and the Earth' , bg.findRandomImg(), bg.findRandomImg())
 sn.createHymn(71, "hymn", bg.findRandomImg())
 sn.createTransition('Announcement', bg.findRandomImg())
 sn.createRecital('Prayer', bg.findRandomImg())
